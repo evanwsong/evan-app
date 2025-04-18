a881dd9baaf0e96f0e26c9ee6a50bc0e60e11cf8
evanwsong <98357945+evanwsong@users.noreply.github.com>
2025-04-18 02:30:28 -0700

    Updates

---
+++ static/js/198.f383baad.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkevan_app=self.webpackChunkevan_app||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.f383baad.chunk.js.map
